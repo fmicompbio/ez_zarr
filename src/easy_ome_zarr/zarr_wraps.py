8f5f003e24e4469e7b1ef7621449c993360c0915
mbstadler <stadler.michael@gmail.com>
2024-01-09 18:33:06 +0100

    refactoring and get_image_rect (no tests yet)

--- src/easy_ome_zarr/zarr_wraps.py
+++ src/easy_ome_zarr/zarr_wraps.py
@@ -23,6 +23,7 @@ import warnings
 class FmiZarr:
     """Represents a ome-zarr fileset."""
 
+    # constructor and helper functions
     def __init__(self, zarr_path, name = None):
         """
         Initializes an ome-zarr fileset (.zarr) from its path.
@@ -50,44 +51,66 @@ class FmiZarr:
         self.label_names = self._load_label_names()
         self.table_names = self._load_table_names()
 
-    def _load_channel_info(self, well = None):
+    def _load_channel_info(self):
         """Load info about available channels."""
-        if not well: # pick first well
-            well = self.wells[0]['path']
+        well = self.wells[0]['path']
         well_group = self.top[os.path.join(well, '0')] # convention: single field of view per well
         if not 'omero' in well_group.attrs or not 'channels' in well_group.attrs['omero']:
             raise ValueError(f"no channel info found in well {well}")
         return well_group.attrs['omero']['channels']
     
-    def _load_multiscale_info(self, well = None):
+    def _load_multiscale_info(self):
         """Load info about available scales."""
-        if not well: # pick first well
-            well = self.wells[0]['path']
+        well = self.wells[0]['path']
         well_group = self.top[os.path.join(well, '0')] # convention: single field of view per well
         if not 'multiscales' in well_group.attrs:
             raise ValueError(f"no multiscale info found in well {well}")
         return well_group.attrs['multiscales'][0]
     
-    def _load_label_names(self, well = None):
+    def _load_label_names(self):
         """Load label names (available segmentations)."""
-        if not well: # pick first well
-            well = self.wells[0]['path']
+        well = self.wells[0]['path']
         label_path = os.path.join(well, '0', 'labels')
         if label_path in self.top:
             return self.top[label_path].attrs['labels']
         else:
             return []
 
-    def _load_table_names(self, well = None):
+    def _load_table_names(self):
         """Load table names (can be extracted using .get_table())."""
-        if not well: # pick first well
-            well = self.wells[0]['path']
+        well = self.wells[0]['path']
         table_path = os.path.join(well, '0', 'tables')
         if table_path in self.top:
             return self.top[table_path].attrs['tables']
         else:
             return []
     
+    # utility functions
+    def _digest_well_argument(self, well = None):
+        """Interpret a single `well` argument in the context of a given FmiZarr object."""
+        return os.path.join(well[:1].upper(), well[1:])
+
+    def _digest_include_wells_argument(self, include_wells = None):
+        """Interpret an `include_wells` argument in the context of a given FmiZarr object."""
+        if not include_wells: 
+            # no wells given -> include all wells
+            include_wells = [x['path'] for x in self.wells]
+        else:
+            # transform well names from 'B03' format to path format 'B/03'
+            include_wells = [self._digest_well_argument(w) for w in include_wells]
+        return include_wells
+
+    def _digest_pyramid_level_argument(self, pyramid_level = None):
+        """Interpret a `pyramid_level` argument in the context of a given FmiZarr object."""
+        if not pyramid_level: 
+            # no pyramid level given -> pick lowest resolution one
+            pyramid_level = self.level_paths[-1]
+        else:
+            # make sure it is an integer
+            pyramid_level = int(pyramid_level)
+        return pyramid_level
+
+    # string representation
     def __str__(self):
         nwells = len(self.wells)
         nch = len(self.channels)
@@ -100,6 +123,7 @@ class FmiZarr:
     def __repr__(self):
         return str(self)
     
+    # slot accessors
     def get_path(self):
         """Gets the path of the ome-zarr fileset."""
         return self.path
@@ -112,13 +136,10 @@ class FmiZarr:
         """Gets info on channels in the ome-zarr fileset."""
         return self.channels
 
+    # query methods
     def get_table(self, table_name, include_wells = None, as_AnnData = False):
         """Extract table for wells in a ome-zarr fileset."""
-        if not include_wells: # include all wells
-            include_wells = [x['path'] for x in self.wells]
-        else:
-            # transform wells in readable ('RowColumn') format to path format: 'Row/Column'
-            include_wells = [item[:1] + '/' + item[1:] for item in include_wells] 
+        include_wells = self._digest_include_wells_argument(include_wells)
 
         table_paths = [os.path.join(w, '0', 'tables', table_name) for w in include_wells]
         # remark: warn if not all well have the table?
@@ -139,6 +160,46 @@ class FmiZarr:
             return anndata_combined
         else:
             return anndata_combined.to_df()
+
+    def get_image_rect(self, well = None, pyramid_level = None,
+                       upper_left = None, lower_right = None, width_height = None,
+                       as_NumPy = False):
+        """
+        Extract a rectangular image region (all z planes if several) from a well by coordinates.
+        None or at least two of `upper_left`, `lower_right` and `width_height` need to be given.
+        If none are given, it will return the full image.
+        Otherwise, `upper_left` contains the lower indices than `lower_right` (origin on the top-left).
+        Each of them is a tuple of (x, y).
+        """
+        # digest arguments
+        well = self._digest_well_argument(well)
+        pyramid_level = self._digest_pyramid_level_argument(pyramid_level)
+
+        # load image (convention: single field of view per well -> '0')
+        img_path = os.path.join(self.path, well, '0', str(pyramid_level))
+        img = dask.array.from_zarr(img_path)
+
+        # calculate corner coordinates and subset if needed
+        # (images are always of 4D shape c,z,y,x)
+        num_unknowns = sum([x == None for x in [upper_left, lower_right, width_height]])
+        if num_unknowns == 1:
+            if width_height:
+                assert all([x > 0 for x in width_height])
+                if not upper_left:
+                    upper_left = tuple(lower_right[i] - width_height[i] for i in range(2))
+                elif not lower_right:
+                    lower_right = tuple(upper_left[i] + width_height[i] for i in range(2))
+            assert all([upper_left[i] < lower_right[i] for i in range(len(upper_left))])
+            img = img[:, :, range(upper_left[1], lower_right[1] + 1), range(upper_left[0], lower_right[0] + 1)]
+        elif num_unknowns != 3:
+            raise ValueError("Either none are two of `upper_left`, `lower_rigth` and `width_height` have to be given")
+
+        # convert if needed and return
+        if as_NumPy:
+            img = np.array(img)
+        return img
+
+
 
 
 
