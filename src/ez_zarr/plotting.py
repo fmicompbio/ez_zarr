388c7d2998340b224145ad51094f55bd40861eac
mbstadler <stadler.michael@gmail.com>
2024-02-04 00:02:42 +0100

    consistently use `im` instead of `data` for image array

--- src/ez_zarr/plotting.py
+++ src/ez_zarr/plotting.py
@@ -47,6 +47,8 @@ def get_shuffled_cmap(cmap_name: str='hsv', seed: int=42) -> mcolors.ListedColor
     shuffled_cmap = mcolors.ListedColormap(all_colors)
     return shuffled_cmap
 
+# helper functions to manipulate image arrays ---------------------------------
+def zproject(im: Union[dask.array.Array, np.ndarray],
              axis: Optional[int]=1,
              method: Optional[str]='maximum',
              keepdims: Optional[bool]=True,
@@ -57,7 +59,7 @@ def get_shuffled_cmap(cmap_name: str='hsv', seed: int=42) -> mcolors.ListedColor
     Helper function to conveniently project multi-dimensional images along z-axis according to desired projection method. 
 
     Parameters:
-        data (dask.array or np.ndarray): The array to be projected
+        im (dask.array or np.ndarray): The array to be projected
         axis (int): The axis on which to perform the projection (typically
             the z-axis). Unless otherwise specified, it defaults to axis 1 (by convention Fractal outputs have shape ch,z,y,x). Note: if masks are projected, Fractal output have usually shape z,y,x, so use `axis=0` in that case.
         method (str): The projection method of choice. Available are 
@@ -73,7 +75,7 @@ def get_shuffled_cmap(cmap_name: str='hsv', seed: int=42) -> mcolors.ListedColor
     Examples:
         Project np.array along z-axis:
 
-        >>> zproject(data=np.array([[[1,2,4],[1,3,5],[1,6,7]],[[1,2,4],[1,3,5],[1,6,7]]]), method='maximum', axis=1)
+        >>> zproject(im=np.array([[[1,2,4],[1,3,5],[1,6,7]],[[1,2,4],[1,3,5],[1,6,7]]]), method='maximum', axis=1)
     """
     methods = {
         'maximum': np.max,
@@ -85,7 +87,7 @@ def get_shuffled_cmap(cmap_name: str='hsv', seed: int=42) -> mcolors.ListedColor
     if not method in methods:
         raise ValueError(f"Unknown method ({method}), should be one of: " + ', '.join(list(methods.keys())))
     
-    im = methods[method](data, axis=axis, keepdims=keepdims) 
+    im = methods[method](im, axis=axis, keepdims=keepdims) 
     if method == 'sum':
         max_value = (2**img_bit) - 1
         im = np.clip(im, 0, max_value)
