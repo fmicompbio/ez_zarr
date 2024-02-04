17ddfe91cf128dbaee18e4fb2712f7bae7f726e0
mbstadler <stadler.michael@gmail.com>
2024-02-04 01:10:46 +0100

    calculate max_yx from well if 'well_ROI_table' is unavailable

--- src/ez_zarr/hcs_wrappers.py
+++ src/ez_zarr/hcs_wrappers.py
@@ -1014,11 +1014,15 @@ class FractalZarr:
 
         # get the maximal well y,x coordinates
         well_tab = self.get_table('well_ROI_table')
-        max_yx = self.convert_micrometer_to_pixel(
-            zyx=(0,
-                 max(well_tab['len_y_micrometer']),
-                 max(well_tab['len_x_micrometer'])),
-            pyramid_level=img_pl)[1:]
+        if well_tab != None:
+            max_yx = self.convert_micrometer_to_pixel(
+                zyx=(0,
+                    max(well_tab['len_y_micrometer']),
+                    max(well_tab['len_x_micrometer'])),
+                pyramid_level=img_pl)[1:]
+        else:
+            well_dims = [self.get_image_ROI(well=w, pyramid_level=img_pl).shape[2:] for w in wells]
+            max_yx = np.max(np.stack(well_dims), axis=0)
 
         # loop over wells
         with plt.style.context('dark_background'):
