ae88b34e97f7b248576f9a5d1a2923541301722c
mbstadler <stadler.michael@gmail.com>
2024-02-05 18:19:13 +0100

    rename convert_to_rgb parameters also in tests

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -73,8 +73,8 @@ def test_convert_to_rgb(npa3d: np.ndarray):
     """Test convert_to_rgb."""
     rng = np.quantile(npa3d[1], [0.01, 0.5])
     rgb = convert_to_rgb(im=npa3d[[0,1]],
-                         colors=['yellow', 'red'],
-                         ranges=[[0.01, 0.5], rng])
+                         channel_colors=['yellow', 'red'],
+                         channel_ranges=[[0.01, 0.5], rng])
     assert isinstance(rgb, np.ndarray)
     assert rgb.shape == (npa3d.shape[2], npa3d.shape[1], 3)
     assert rgb.dtype == np.uint8
