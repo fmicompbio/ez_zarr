95021b7a8fbcdc50fb1dbee8f03a2cfc4066249a
mbstadler <stadler.michael@gmail.com>
2024-05-02 18:20:47 +0200

    add helper function to convert coordinates between scales
    Co-authored-by: Silvia Barbiero <silvia.barbiero@gmail.com>
    Co-authored-by: Charlotte Soneson <charlottesoneson@gmail.com>
    Co-authored-by: Michael Stadler <stadler.michael@gmail.com>

--- src/ez_zarr/ome_zarr.py
+++ src/ez_zarr/ome_zarr.py
@@ -195,3 +195,37 @@ class Image:
     def __repr__(self):
         return str(self)
     
+    # coordinate conversion ---------------------------------------------------
+    ## REMARK: could be moved to utils.py
+    @staticmethod
+    def convert_coordinates(coords_from: tuple[Union[int, float]],
+                            scale_from: list[float],
+                            scale_to: list[float]) -> tuple[Union[int, float]]:
+        """
+        Convert coordinates between scales.
+
+        Parameters:
+            coords_from (tuple): The coordinates to be converted.
+            scale_from (list[float]): The scale that the coordinates refer to.
+                Needs to be parallel to `coords_from`.
+            scale_to (list[float]): The scale that the coordinates should be
+                converted to.
+        
+        Returns:
+            A tuple of the same length as `coords_from` with coordinates in
+            the new scale.
+        
+        Examples:
+            Convert a point (10, 30) from [1, 1] to [2, 3]:
+
+            >>> y, x = img.convert_coordinates((10,30), [1, 1], [2, 3])
+        """
+        # digest arguments
+        assert len(coords_from) == len(scale_from)
+        assert len(coords_from) == len(scale_to)
+
+        # convert and return
+        coords_to = np.divide(np.multiply(coords_from, scale_from), scale_to)
+        return(tuple(coords_to))
+
+    # accessor methods -----------------------------------------------------------
